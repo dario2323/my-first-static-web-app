67179c36d3f2b8ac47ad3ab32e34794057a7b20b
dario.tubon <dario.tubon@calidadsalud.gob.ec>
2022-02-18 09:52:40 -0500

    Agrega comonente /home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { HomeComponent } from './components/home/home.component';
+import { APP_ROUTING } from "./app.route";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, APP_ROUTING],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
